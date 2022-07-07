703e3f07ba9b7fa742c42caa290cecaafbad4d57
meshal1172 <mr.meeshal@hotmail.com>
2022-07-07 14:22:22 +0300

    tes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 // function x() {}
 // import {Book } from '@prisma/client';
+
+require('dotenv').config();
+const Discord = require('discord.js')
+
 import { connectDb, prismaClient } from './prisma';
 import { listen } from './server';
 
